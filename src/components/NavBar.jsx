a652e526a67d5def7a59965e0c65dfc9f52d6dc1
Joaco <71678926+TheYiro@users.noreply.github.com>
2024-06-13 15:12:03 -0500

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
     return (
         <div className="navBar">
-        <Link to='/'><img src="/src/assets/joacodevlogo.webp" alt="Joaco Logo" /></Link>
+        <Link to='/'><img src="https://cdn.discordapp.com/attachments/770877999599190056/1250905468201734376/joacodevlogo.webp?ex=666ca400&is=666b5280&hm=1bb2e7cbce1650dd1d2cfb4e23879f4287f7d0a2e0dbe28b9678f03ccaa05b78&" alt="Joaco Logo" /></Link>
         </div>
         );
 }
